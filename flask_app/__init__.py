0368b85f74eaaa7fbed6f2631ec55320966a3b3a
William Ma <mr8bit@wireless-10-104-229-93.umd.edu>
2022-05-11 03:53:18 -0400

    a

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -31,11 +31,13 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-def create_app(a, b):
+def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config.from_pyfile("config.py", silent=False)
     app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
+    if test_config is not None:
+        app.config.update(test_config)
 
     db.init_app(app)
     login_manager.init_app(app)
